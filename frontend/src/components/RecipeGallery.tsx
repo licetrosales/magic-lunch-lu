d6d2fabeb81fb5f9efbf0a52ec206e84d5167368
licetullmann <licetrosales@gmail.com>
2022-12-23 17:40:02 +0100

    Create RecipeGallery & GET recipe request function & use useEffect to avoid infinite loop

---
+++ frontend/src/components/RecipeGallery.tsx
@@ -0,0 +1,18 @@
+import {Recipe} from "../model/Recipe";
+import RecipeCardGalleryView from "./RecipeCardGalleryView";
+
+type RecipeGalleryProps = {
+    recipesToMap: Recipe []
+}
+
+export default function RecipeGallery(props: RecipeGalleryProps) {
+
+    const recipeItemComponents = props.recipesToMap.map(recipeShortInfo => {
+        return <RecipeCardGalleryView recipeToDisplay={recipeShortInfo}/>
+    })
+    return (
+        <section>
+            {recipeItemComponents}
+        </section>
+    )
+}
