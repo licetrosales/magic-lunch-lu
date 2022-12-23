662fb38c4f8a79c6a613a66ca2a57e943a94e26f
licetullmann <licetrosales@gmail.com>
2022-12-23 17:40:02 +0100

    Create RecipeGallery & GET recipe request function & use useEffect to avoid infinite loop

--- frontend/src/components/MyMagicLunchApp.tsx
+++ frontend/src/components/MyMagicLunchApp.tsx
@@ -1,8 +1,31 @@
+import RecipeGallery from "./RecipeGallery";
+import {useEffect, useState} from "react";
+import {Recipe} from "../model/Recipe";
+import axios from "axios";
+
 export default function MyMagicLunchApp() {
+
+    const [recipes, setRecipes] = useState<Recipe[]>([])
+    useEffect(() => {
+        getRecipes()
+    },[])
+
+    const recipeBaseUrl="/api/users/userId/recipes"
+    function getRecipes(){
+        axios.get(recipeBaseUrl)
+            .then(recipeGalleryResponse =>{
+                const newRecipeGallery : Recipe [] = recipeGalleryResponse.data;
+                setRecipes(newRecipeGallery);
+            })
+            .catch(errorMessageResponse => {
+                console.log("There is an error by get request: " + errorMessageResponse)
+            })
+    }
 
     return (
         <section>
             <h1>Meine Rezepte</h1>
+            <RecipeGallery recipesToMap={recipes}/>
         </section>
     )
 
