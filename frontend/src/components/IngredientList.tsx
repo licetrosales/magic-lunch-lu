b303417b82b435661547a21dd7881128bebbad69
licetullmann <licetrosales@gmail.com>
2022-12-28 17:59:43 +0100

    Add IngredientList component

---
+++ frontend/src/components/IngredientList.tsx
@@ -0,0 +1,11 @@
+import {Container} from "@mui/material";
+
+export default function IngredientList(){
+    return (
+        <div className = "Ingridient-list">
+            <Container>
+
+            </Container>
+        </div>
+    )
+}
