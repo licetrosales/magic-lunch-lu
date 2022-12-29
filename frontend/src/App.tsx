a68c5775433ddba5f67de8fa27d153948bb5631c
licetullmann <licetrosales@gmail.com>
2022-12-29 21:24:48 +0100

    Add react-router-dom library & Startseite comp.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import MyMagicLunchApp from "./components/MyMagicLunchApp";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Startseite from "./components/Startseite";
 
 function App() {
     return (
         <div className="App">
-            <MyMagicLunchApp/>
+            <BrowserRouter>
+                <nav>
+                    <Link to={"/"}>Startseite</Link>
+                    <Link to={"/recipes"}>Meine Rezepte</Link>{' '}
+                </nav>
+                <Routes>
+                    <Route path={"/"} element={<Startseite/>}></Route>
+                    <Route path={"/recipes"} element={<MyMagicLunchApp/>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ frontend/src/components/Startseite.tsx
@@ -0,0 +1,9 @@
+import {Typography} from "@mui/material";
+
+export default function Startseite() {
+    return (
+        <Typography variant={"h6"}>
+            Startseite
+        </Typography>
+    )
+}
