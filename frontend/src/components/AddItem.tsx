4abce6ff116daefe6c0d776d95182f4487d5cff0
licetullmann <licetrosales@gmail.com>
2023-01-03 11:00:40 +0100

    Add ingrediens to CreateRecipeForm

--- frontend/src/components/AddItem.tsx
+++ frontend/src/components/AddItem.tsx
@@ -1,92 +1,107 @@
 import {ChangeEvent, useState} from "react";
 
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@mui/material";
-import {Ingredient} from "../model/Ingredient";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField} from "@mui/material";
+import {Ingredient, NewItem, ProductCategory, Unit} from "../model/Ingredient";
+import {MealType, MenuCategory} from "../model/Recipe";
 
-type AddItemProps ={
-    handleAddItem(newItem: Ingredient):void
+type AddItemProps = {
+    handleAddItem(newItem: Ingredient): void
 }
-export type NewItemType ={
-    itemName: string,
-    quantity: string,
-    unit: "TL"|"EL"|"MSP"|"OZ"|"LB"|"G"|"KG"|"KARTON"|"BUND"| "PACKUNG"|"ZWEIG"|"KLEIN"| "MITTEL"| "",
-    productCategory: "BREAD_BAKERY"|"BEVERAGES"|"CANNED_JARED_GOODS"|"DAIRY"|"PRODUCE"|"DRY_BACKING_GOODS"|"FROZEN_FOODS"|"MEAT"|"PERSONAL_CARE"|"OTHERS"|"",
 
-
-}
-export default function AddItem(props: AddItemProps){
-    const emptyFormItem: NewItemType = {
+export default function AddItem(props: AddItemProps) {
+    const emptyItemFormWithoutEnums: NewItem = {
         itemName: "",
         quantity: "",
-        unit: "",
-        productCategory:"",
+        isInShoppingList: true
     }
+    const [itemWithoutEnums, setItemWithoutEnums] = useState(emptyItemFormWithoutEnums)
+    const [unit, setUnit] = useState<Unit | string>(Unit.KG)
 
     const [open, setOpen] = useState<boolean>(false)
-    const [item, setItem] = useState<NewItemType>(emptyFormItem)
 
-
-    function handleOpen(){
+    function handleOpen() {
         setOpen(true)
     }
-    function  handleClose(){
+
+    function handleClose() {
         setOpen(false)
     }
-    function handleChange(event:ChangeEvent<HTMLInputElement>){
-        const fieldName= event.target.name
-        const fieldValue= event.target.value
 
-            setItem((prevItem) =>({
-                ...prevItem,[fieldName]:fieldValue
-            }))
-
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        const fieldName = event.target.name
+        const fieldValue = event.target.value
+        setItemWithoutEnums((prevItem) => ({
+            ...prevItem, [fieldName]: fieldValue
+        }))
     }
-    function handleAddItem(){
+
+    function onUnitChange(event: ChangeEvent<HTMLInputElement>) {
+        setUnit(event.target.value as Unit)
+    }
+
+    function handleAddItem() {
         const newItem: Ingredient = {
-            nameItem: item.itemName,
-            quantity: item.quantity,
-            unit: item.unit,
-            productCategory: item.productCategory
+            itemName: itemWithoutEnums.itemName,
+            quantity: itemWithoutEnums.quantity,
+            unit: unit,
 
         }
         props.handleAddItem(newItem)
-        setItem({ itemName: "",
+        setItemWithoutEnums({
+            itemName: "",
             quantity: "",
-            unit: "",
-            productCategory:""
-            })
+            isInShoppingList: true
+        })
+        setUnit(Unit.KG)
         handleClose()
     }
+
     return (
         <div>
-            <Button onClick={handleOpen}>+ Zutat</Button>
-          <Dialog open={open} onClose={handleClose}>
-              <DialogTitle>Neue Zutat</DialogTitle>
-              <DialogContent>
-                <TextField value={item.itemName}
-                           margin={"dense"}
-                           onChange={handleChange}
-                           name={"itemName"}
-                           label = "Name"
-                           fullWidth
-                />
-                  <TextField value={item.quantity}
-                             margin={"dense"}
-                             onChange={handleChange}
-                             name={"quantity"}
-                             label={"Quantity"}
-                             fullWidth
-                  />
-              </DialogContent>
-              <DialogActions>
-                  <Button onClick={handleClose}>
-                      Cancel
-                  </Button>
-                  <Button onClick={handleAddItem}>
-                      Speichern
-                  </Button>
-              </DialogActions>
-          </Dialog>
+            <Button onClick={handleOpen}>Zutaten hizufügen</Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Neue Zutat</DialogTitle>
+                <DialogContent>
+                    <TextField value={itemWithoutEnums.itemName}
+                               margin={"dense"}
+                               onChange={handleChange}
+                               name={"itemName"}
+                               label="Name"
+                               fullWidth
+                    />
+                    <TextField value={itemWithoutEnums.quantity}
+                               margin={"dense"}
+                               onChange={handleChange}
+                               name={"quantity"}
+                               label={"Quantity"}
+                               fullWidth
+                    />
+                    <TextField
+                        select
+                        name={"unit"}
+                        value={unit}
+                        label={"Unit"}
+                        margin={"dense"}
+                               onChange={onUnitChange}
+                        fullWidth
+                    >
+                        <MenuItem value={Unit.KG}>kg</MenuItem>
+                        <MenuItem value={Unit.G}>g</MenuItem>
+                        <MenuItem value={Unit.LB}>lb</MenuItem>
+                        <MenuItem value={Unit.OZ}>oz</MenuItem>
+                        <MenuItem value={Unit.OTHER}>other</MenuItem>
+                    </TextField>
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button onClick={handleAddItem}>
+                        Speichern
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
