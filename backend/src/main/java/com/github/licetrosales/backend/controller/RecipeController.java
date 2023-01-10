5f069138a632a386301b88484c3227b8493b1924
licetullmann <licetrosales@gmail.com>
2023-01-10 20:52:55 +0100

    Add RecipeController for update function

--- backend/src/main/java/com/github/licetrosales/backend/controller/RecipeController.java
+++ backend/src/main/java/com/github/licetrosales/backend/controller/RecipeController.java
@@ -5,9 +5,13 @@ import com.github.licetrosales.backend.model.RecipeDTO;
 import com.github.licetrosales.backend.repo.RecipeRepo;
 import com.github.licetrosales.backend.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Update.update;
 
 @RestController
 @RequestMapping("/api/users/userId")
@@ -37,4 +41,11 @@ public class RecipeController {
         recipeService.delete(id);
     }
 
+    @PutMapping(path = "/recipes/{id}")
+    Recipe updateRecipe(@PathVariable String id, @RequestBody Recipe recipeToUpdate) {
+        if (!recipeToUpdate.id().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The id in the url does not match the request body's id");
+        }
+        return recipeService.updateRecipe(recipeToUpdate);
+    }
 }
