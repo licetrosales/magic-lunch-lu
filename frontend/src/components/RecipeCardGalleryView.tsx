8f3961d83aa8c0858b72bb0893be87cbd5f8a1c5
licetullmann <licetrosales@gmail.com>
2023-01-10 23:10:16 +0100

    Add modal dialog for RecipeCard

--- frontend/src/components/RecipeCardGalleryView.tsx
+++ frontend/src/components/RecipeCardGalleryView.tsx
@@ -1,7 +1,8 @@
 import {Recipe} from "../model/Recipe";
 import "../images/BigMacSalat.jpg"
-import {Button} from "@mui/material";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle} from "@mui/material";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 type RecipeCardGalleryViewProps = {
     recipeToDisplay: Recipe
@@ -10,16 +11,27 @@ type RecipeCardGalleryViewProps = {
 export default function RecipeCardGalleryView(props: RecipeCardGalleryViewProps) {
 
     const navigate = useNavigate()
+    const [open, setOpen] = useState<boolean>(false)
+
     function onDeleteClick() {
         props.recipeToRemove(props.recipeToDisplay.id)
     }
+    function handleOpen() {
+        setOpen(true)
+    }
+    function handleClose() {
+        setOpen(false)
+    }
     function onDetailsClick() {
-        navigate("/recipes/" + props.recipeToDisplay.id)
+        //navigate("/recipes/" + props.recipeToDisplay.id)
+        handleOpen()
+    }
+    function updateRecipe(){
+
     }
 
     return (
         <div>
-
             <h3>{props.recipeToDisplay.name}</h3>
             <img alt="photo" src={props.recipeToDisplay.image}/>
             <p>{props.recipeToDisplay.mealType} </p>
@@ -27,6 +39,21 @@ export default function RecipeCardGalleryView(props: RecipeCardGalleryViewProps)
             <p>{props.recipeToDisplay.favorite} </p>
             <Button onClick={onDeleteClick} variant="outlined">Löschen</Button>
             <Button onClick={onDetailsClick} variant="outlined">Details</Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Rezept</DialogTitle>
+                <DialogContent>
+                    <h3>{props.recipeToDisplay.name}</h3>
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={updateRecipe}>
+                        Ändern
+                    </Button>
+                    <Button onClick={handleClose}>
+                        Schließen
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 
