76ce8f1a1ff3e4c1a2488db5c031dcb926acbb11
licetullmann <licetrosales@gmail.com>
2023-01-15 03:14:11 +0100

    Add Header component to the AppBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import MyMagicLunchApp from "./components/MyMagicLunchApp";
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
-import Startseite from "./components/Startseite";
+import Home from "./components/Home";
+import WeekPlanner from "./components/WeekPlanner";
+
 
 function App() {
     return (
@@ -11,12 +13,15 @@ function App() {
                 <nav>
                     <Link to={"/"}>Startseite</Link>
                     <Link to={"/recipes"}>Meine Rezepte</Link>{' '}
+                    <Link to={"/weekplan-history"}>Wochenplanchronik</Link>{' '}
                 </nav>
                 <Routes>
-                    <Route path={"/"} element={<Startseite/>}></Route>
+                    <Route path={"/"} element={<Home/>}></Route>
                     <Route path={"/recipes"} element={<MyMagicLunchApp/>}></Route>
+                    <Route path={"/weekplan-history"} element={<WeekPlanner/>}></Route>
                 </Routes>
             </BrowserRouter>
+
         </div>
     );
 }
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,18 @@
+import "./Home.style.css"
+
+export default function Home() {
+    return (
+        <div>
+            <div className={"main-header"}>
+                <header>
+                    <h1>Magic Lunch</h1>
+                </header>
+            </div>
+            <section className={"section-content "} >
+                <div>
+                    Login section
+                </div>
+            </section>
+        </div>
+    )
+}
