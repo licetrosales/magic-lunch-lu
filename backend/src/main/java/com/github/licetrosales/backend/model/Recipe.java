c2325c4f46f89d283624d446baf169bb1f9b24ad
licetullmann <licetrosales@gmail.com>
2023-01-16 20:50:39 +0100

    Remove unused constructor in recipe

--- backend/src/main/java/com/github/licetrosales/backend/model/Recipe.java
+++ backend/src/main/java/com/github/licetrosales/backend/model/Recipe.java
@@ -26,19 +26,4 @@ public record Recipe(
         MenuCategory menuCategory,
         String garnish
 ) {
-    public Recipe(
-            String name,
-            MealType mealType,
-            String source
-    ) {
-        this(null, name, mealType, source, null, Collections.emptyList(), null, null, 0, null, null, null, null, null);
-    }
-   public Recipe(
-            String id,
-            String name,
-            MealType mealType,
-            String source
-    ) {
-        this(id, name, mealType, source, null, Collections.emptyList(), null, null, 0, null, null, null, null, null);
-    }
 }
