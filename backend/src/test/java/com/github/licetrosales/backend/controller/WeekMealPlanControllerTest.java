b4a30e0e00a4645d5ffc40cddbc7b1750f304e60
licetullmann <licetrosales@gmail.com>
2023-01-23 22:53:25 +0100

    Add test for function get for week meal plans

---
+++ backend/src/test/java/com/github/licetrosales/backend/controller/WeekMealPlanControllerTest.java
@@ -0,0 +1,67 @@
+package com.github.licetrosales.backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.licetrosales.backend.model.WeekMealPlan;
+import com.github.licetrosales.backend.repo.WeekMealPlanRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class WeekMealPlanControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    WeekMealPlanRepo weekMealPlanRepo;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext
+    void getAllWeekMealPlans_shouldReturnEmptyList_whenWeekMealPlanGalleryisEmpty() throws Exception {
+        mockMvc.perform(get("/api/users/userId/mealplans"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                         []
+                        """));
+    }@Test
+    @DirtiesContext
+    void addWeekMealPlan__shouldReturnWeekMealPlanSentWithPost_whenPostRequestIsSuccessful() throws Exception {
+
+        MvcResult response = mockMvc.perform(post("/api/users/userId/mealplans")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                """
+                                                {
+                                                   "id": "Today",
+                                                   "weekMealPlan": [{}]
+                                                  }
+                                        """
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = response.getResponse().getContentAsString();
+        WeekMealPlan result = objectMapper.readValue(content, WeekMealPlan.class);
+        WeekMealPlan expected = new WeekMealPlan(result.id(), result.weekMealPlan());
+        assertEquals(expected, result);
+    }
+}
+
+
+
+
+
+
+
