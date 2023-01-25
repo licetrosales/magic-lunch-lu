c67bba9b29b81215903b2f5ef50f36a1f57cbf35
licetullmann <licetrosales@gmail.com>
2023-01-25 17:05:50 +0100

    Fix format

--- frontend/src/components/WeekMealPlanModalDialog.tsx
+++ frontend/src/components/WeekMealPlanModalDialog.tsx
@@ -4,12 +4,14 @@ import {Meal} from "../model/Meal";
 import {useState} from "react";
 import {WeekMealPlan} from "../model/WeekMealPlan";
 import LocalDiningIcon from "@mui/icons-material/LocalDining";
+
 type WeekMealPlanModalialogProps = {
 
     weekMealPlan: Meal []
 }
-export default function WeekMealPlanModalDialog (props: WeekMealPlanModalialogProps){
+export default function WeekMealPlanModalDialog(props: WeekMealPlanModalialogProps) {
     const [openMealPlan, setOpenMealPlan] = useState<boolean>(false)
+
     function handleOpenMealPlan() {
         setOpenMealPlan(true)
     }
@@ -17,22 +19,23 @@ export default function WeekMealPlanModalDialog (props: WeekMealPlanModalialogPr
     function handleCloseMealPlan() {
         setOpenMealPlan(false)
     }
-    console.log(props.weekMealPlan)
-return(props.weekMealPlan && props.weekMealPlan.length > 0 ?
-<>    <Button onClick={handleOpenMealPlan} color={"secondary"} variant={"outlined"}
-            startIcon={<LocalDiningIcon/>}/>
 
-    <Dialog open={openMealPlan} onClose={handleCloseMealPlan}>
-        <DialogTitle>Wocheplan</DialogTitle>
-        <DialogContent>
-            <RecipeWeekMealPlanGallery mealsToMap={props.weekMealPlan}/>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={handleCloseMealPlan} color={"secondary"}>
-                Schließen
-            </Button>
-        </DialogActions>
-    </Dialog>
-</>: <p>Modal dialog do not show recipes  </p>
-)
+    console.log(props.weekMealPlan)
+    return (props.weekMealPlan && props.weekMealPlan.length > 0 ?
+            <>    <Button onClick={handleOpenMealPlan} color={"secondary"} variant={"outlined"}
+                          startIcon={<LocalDiningIcon/>}/>
+
+                <Dialog open={openMealPlan} onClose={handleCloseMealPlan}>
+                    <DialogTitle>Wocheplan</DialogTitle>
+                    <DialogContent>
+                        <RecipeWeekMealPlanGallery mealsToMap={props.weekMealPlan}/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseMealPlan} color={"secondary"}>
+                            Schließen
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </> : <p>Modal dialog do not show recipes </p>
+    )
 }
